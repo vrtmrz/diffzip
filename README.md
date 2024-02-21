13b8283bce2fa8c14dd3060fc0b99d72cfa76abe
vorotamoroz <vrtmrz@gmail.com>
2024-02-21 08:16:23 +0000

    New features

--- README.md
+++ README.md
@@ -4,12 +4,34 @@ This is a vault backup plugin for [Obsidian](https://obsidian.md).
 
 We can store all the files which have been modified, into a ZIP file.
 
+## Installation
+
+1. Install this plug-in from [Beta Reviewers Auto-update Tester](https://github.com/TfTHacker/obsidian42-brat).
+
 ## How to use
-1. Install this plug-in by [Beta Reviewers Auto-update Tester](https://github.com/TfTHacker/obsidian42-brat).
-2. Make a `backup` folder on your vault.
-  Note: We can change it on the setting dialogue.
-3. Perform `Create Differential Backup ZIP` from the command palette.
-4. We will get `backupinfo.md` and a zip file `YYYY-MM-DD-SECONDS.zip`
+
+### Making backup
+1. Perform `Create Differential Backup` from the command palette.
+2. We will get `backupinfo.md` and a zip file `YYYY-MM-DD-SECONDS.zip` in the `backup` folder
+   - `backup` folder can be configured in the settings dialogue.
+
+### Restore a file
+1. Perform `Restore from backups` from the command palette.
+2. Select the file you want to restore.
+3. Select the backup you want to restore.
+4. Select the place to save the restored file.
+5. We got an old file.
+
+## Settings
+
+| Key                    | Description                                                                                                                                                        |
+| ---------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------ |
+| Start backup at launch | When the plug-in has been loaded, Differential backup will be created automatically.                                                                               |
+| Backup folder          | The folder which backups are stored.                                                                                                                               |
+| Restore folder         | The folder which restored files will be stored.                                                                                                                    |
+| Include hidden folder  | Backup also the configurations, plugins, themes, and, snippets.                                                                                                    |
+| ZIP splitting size     | An large file are not good for handling, so this plug-in splits the backup ZIP into this size. This splitted ZIP files can be handled by 7Z or something archiver. |
+
 
 ## What is `backupinfo.md`?
 
@@ -23,39 +45,48 @@ Untitled.md:
   digest: 452438bd53ea864cdf60269823ea8222366646c14f0f1cd450b5df4a74a7b19b
   filename: Untitled.md
   mtime: 1703656274225
-  storedIn: 2023-12-28-41265.zip
+  history:
+    - zipName: 2023-12-28-41265.zip
+      modified: 2023-12-27T05:51:14.225Z
+  storedIn: 
 Untitled 2.md:
   digest: 7241f90bf62d00fde6e0cf2ada1beb18776553ded5233f97f0be3f7066c83530
   filename: Untitled 2.md
-  mtime: 1703730478953
-  storedIn: 2023-12-28-41283.zip
+  mtime: 1703656274225
+  history:
+    - zipName: 2023-12-28-41265.zip
+      modified: 2023-12-27T05:51:14.225Z
 Untitled 1.md:
   digest: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
   filename: Untitled 1.md
-  mtime: 1703656280045
-  storedIn: 2023-12-28-41265.zip
+  mtime: 1708498190402
+  history:
+    - zipName: 2023-12-28-41265.zip
+      modified: 2023-12-27T05:51:14.225Z
+    - zipName: 2024-2-21-56995.zip
+      modified: 2024-02-21T06:49:50.402Z
 ```
 
 The following entries are important.
 
-| key      | value                                                    |
-| -------- | -------------------------------------------------------- |
-| digest   | SHA-1 of the file. DZB detects all changes by this hash. |
-| storedIn | ZIP file name that the file has been stored.             |
+| key     | value                                                    |
+| ------- | -------------------------------------------------------- |
+| digest  | SHA-1 of the file. DZB detects all changes by this hash. |
+| history | Archived ZIP file name and Timestamp at the time.        |
 
 Note: Modified time has been stored due to the lack of resolution of the ZIP file, but this is information for us.
 
 ### ZIP files
 We will get the following zip files.
 
-| 2023-12-28-41265.zip | 2023-12-28-41283.zip |
-| -------------------- | -------------------- |
-| Untitled.md          |                      |
-| Untitled 1.md        |                      |
-| Untitled 2.md        | Untitled 2.md        |
-| backupinfo.md        | backupinfo.md        |
+| 2023-12-28-41265.zip | 2024-2-21-56995.zip |
+| -------------------- | ------------------- |
+| Untitled.md          |                     |
+| Untitled 1.md        |                     |
+| Untitled 2.md        | Untitled 1.md       |
+| backupinfo.md        | backupinfo.md       |
 
-As the astute will have noticed, we can pick the ZIP which contains the file we want from only the latest one!
+As the astute will have noticed, we can pick the ZIP that contains the file we want from only the latest one without any special tool!
 
 ---
 License: MIT
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import {
 	App,
+	FuzzySuggestModal,
 	Notice,
 	Plugin,
 	PluginSettingTab,
@@ -13,18 +14,24 @@ import * as fflate from "fflate";
 
 interface DZBSettings {
 	backupFolder: string;
+	restoreFolder: string;
 	maxSize: number;
+	startBackupAtLaunch: boolean;
+	includeHiddenFolder: boolean;
 }
 const InfoFile = `backupinfo.md`;
 const DEFAULT_SETTINGS: DZBSettings = {
+	startBackupAtLaunch: false,
 	backupFolder: "backup",
+	restoreFolder: "restored",
+	includeHiddenFolder: false,
 	maxSize: 30,
 };
 
 type FileInfo = {
 	filename: string;
 	digest: string;
-	storedIn: string;
+	history: { zipName: string, modified: string }[];
 	mtime: number;
 };
 type FileInfos = Record<string, FileInfo>;
@@ -41,6 +48,50 @@ async function computeDigest(data: Uint8Array) {
 		.map((b) => b.toString(16).padStart(2, "0"))
 		.join("");
 	return hashHex;
+}
+
+
+async function ensureDirectory(app: App, fullpath: string) {
+	const pathElements = fullpath.split("/");
+	pathElements.pop();
+	let c = "";
+	for (const v of pathElements) {
+		c += v;
+		try {
+			await app.vault.createFolder(c);
+		} catch (ex) {
+			// basically skip exceptions.
+			if (ex.message && ex.message == "Folder already exists.") {
+				// especialy this message is.
+			} else {
+				new Notice("Folder Create Error");
+				console.log(ex);
+			}
+		}
+		c += "/";
+	}
+}
+
+async function getFiles(
+	app: App,
+	path: string,
+	ignoreList: string[]
+) {
+	const w = await app.vault.adapter.list(path);
+	let files = [
+		...w.files
+			.filter((e) => !ignoreList.some((ee) => e.endsWith(ee)))
+	];
+	L1: for (const v of w.folders) {
+		for (const ignore of ignoreList) {
+			if (v.endsWith(ignore)) {
+				continue L1;
+			}
+		}
+		// files = files.concat([v]);
+		files = files.concat(await getFiles(app, v, ignoreList));
+	}
+	return files;
 }
 
 export default class DiffZipBackupPlugin extends Plugin {
@@ -77,14 +128,13 @@ export default class DiffZipBackupPlugin extends Plugin {
 		console.log(`${dt}\t${message}`);
 	}
 
-	async createZip() {
-		const allFiles = [...this.app.vault.getFiles()];
+	async loadTOC() {
 		let toc = {} as FileInfos;
 		const indexFile = this.app.vault.getAbstractFileByPath(
 			normalizePath(`${this.settings.backupFolder}/${InfoFile}`)
 		);
 		if (indexFile && indexFile instanceof TFile) {
-			this.logMessage(`Loading Backup information`, "proc-index");
+			this.logWrite(`Loading Backup information`, "proc-index");
 			try {
 				const tocStr = await this.app.vault.read(indexFile);
 				toc = parseYaml(tocStr.replace(/^```$/gm, ""));
@@ -95,7 +145,7 @@ export default class DiffZipBackupPlugin extends Plugin {
 					);
 					toc = {};
 				} else {
-					this.logMessage(
+					this.logWrite(
 						`Backup information has been loaded`,
 						"proc-index"
 					);
@@ -111,6 +161,70 @@ export default class DiffZipBackupPlugin extends Plugin {
 		} else {
 			this.logMessage(`Backup information looks missing`, "proc-index");
 		}
+		return toc;
+	}
+	async readFile(filename: string) {
+		if (this.settings.includeHiddenFolder) {
+			return await this.app.vault.adapter.readBinary(filename);
+		} else {
+			const f = this.app.vault.getAbstractFileByPath(filename);
+			if (f instanceof TFile) {
+				return await this.app.vault.readBinary(f);
+			}
+		}
+		return null;
+	}
+	async readStat(filename: string) {
+		if (this.settings.includeHiddenFolder) {
+			return await this.app.vault.adapter.stat(filename);
+		} else {
+			const f = this.app.vault.getAbstractFileByPath(filename);
+			if (f instanceof TFile) {
+				return f.stat;
+			}
+		}
+		return null;
+	}
+	async writeFileBinary(filename: string, content: ArrayBuffer) {
+		await ensureDirectory(this.app, filename);
+		await this.app.vault.adapter.writeBinary(filename, content);
+		return true;
+	}
+	async writeFile(filename: string, content: ArrayBuffer) {
+		try {
+			const f = this.app.vault.getAbstractFileByPath(filename);
+			if (f instanceof TFile) {
+				await this.app.vault.modifyBinary(f, content);
+				return true;
+			} else if (f == null) {
+				// If it could not get by getAbstractFileByPath, try adapter function once.
+				try {
+					const stat = await this.app.vault.adapter.stat(filename);
+					if (stat?.type == "file") {
+						return await this.writeFileBinary(filename, content);
+					} else if (stat?.type == "folder") {
+						return false;
+					}
+				} catch (ex) {
+					//NO OP.
+				}
+				await ensureDirectory(this.app, filename);
+				await this.app.vault.createBinary(filename, content)
+				return true;
+
+			}
+		} catch (ex) {
+			console.dir(ex);
+		}
+		return false;
+	}
+
+	async createZip(verbosity: boolean) {
+		const log = verbosity ? (msg: string, key?: string) => this.logWrite(msg, key) : (msg: string, key?: string) => this.logMessage(msg, key);
+		const ignoreDirs = ["node_modules", ".git", this.app.vault.configDir + "/trash", this.app.vault.configDir + "/workspace.json", this.app.vault.configDir + "/workspace-mobile.json"];
+		const allFiles = this.settings.includeHiddenFolder ? await getFiles(this.app, "", ignoreDirs) : this.app.vault.getFiles().map(e => e.path);
+		// const allFiles = [...this.app.vault.getFiles()];
+		const toc = await this.loadTOC();
 		const today = new Date();
 		const secondsInDay =
 			~~(today.getTime() / 1000 - today.getTimezoneOffset() * 60) % 86400;
@@ -143,6 +257,7 @@ export default class DiffZipBackupPlugin extends Plugin {
 							this.settings.backupFolder + "/" + newFileName + (pieceCount == 0 ? "" : ("." + (`00${pieceCount}`.slice(-3))))
 						);
 						pieceCount++;
+						await ensureDirectory(this.app, outZipFile);
 						this.app.vault.createBinary(
 							outZipFile,
 							buf.slice(i, i + step)
@@ -150,7 +265,7 @@ export default class DiffZipBackupPlugin extends Plugin {
 						i += step;
 						this.logMessage(
 							`${outZipFile} has been created!`,
-							"proc-zip-archive"
+							"proc-zip-process"
 						);
 					}
 					const tocFilePath = normalizePath(
@@ -167,48 +282,61 @@ export default class DiffZipBackupPlugin extends Plugin {
 							`\`\`\`\n${stringifyYaml(toc)}\n\`\`\`\n`
 						);
 					}
-					this.logMessage(`Backup information has been updated`);
+					log(`Backup information has been updated`);
 				}
 			}
 		});
 		const normalFiles = allFiles.filter(
-			(e) => !e.path.startsWith(this.settings.backupFolder + "/")
+			(e) => !e.startsWith(this.settings.backupFolder + "/") && !e.startsWith(this.settings.restoreFolder + "/")
 		);
 		let processed = 0;
 		let zipped = 0;
 
-		for (const file of normalFiles) {
+		for (const path of normalFiles) {
 			this.logMessage(
-				`Processing:${file.path} ${processed}/${normalFiles.length}`,
+				`Backup processing ${processed}/${normalFiles.length}  ${verbosity ? `\n${path}` : ""}`,
 				"proc-zip-process"
 			);
-			const path = file.path;
-			const f = new Uint8Array(await this.app.vault.readBinary(file));
+			// const path = file;
+			const content = await this.readFile(path);
+			if (!content) {
+				this.logMessage(
+					`Archiving:Could not read ${path}`,
+				);
+				continue;
+			}
+			const f = new Uint8Array(content);
 			const digest = await computeDigest(f);
 			processed++;
 			if (path in toc) {
-				this.logWrite("Already in index");
 				const entry = toc[path];
 				if (entry.digest == digest) {
 					this.logWrite(
-						`old file digest:${entry.digest}, this:${digest}, not changed`
+						`${path} Not changed`
 					);
 					continue;
 				}
 			}
 			zipped++;
+			const stat = await this.readStat(path);
+			if (!stat) {
+				this.logMessage(
+					`Archiving:Could not read stat ${path}`,
+				);
+				continue;
+			}
 			toc[path] = {
 				digest,
 				filename: path,
-				mtime: file.stat.mtime,
-				storedIn: newFileName,
+				mtime: stat.mtime,
+				history: [...toc[path]?.history ?? [], { zipName: newFileName, modified: new Date(stat.mtime).toISOString() }],
 			};
-			const fflateFile = new fflate.ZipDeflate(file.path, {
+			const fflateFile = new fflate.ZipDeflate(path, {
 				level: 9,
 			});
-			fflateFile.mtime = file.stat.mtime;
+			fflateFile.mtime = stat.mtime;
 			this.logMessage(
-				`Archiving:${file.path} ${zipped}/${normalFiles.length}`,
+				`Archiving:${path} ${zipped}/${normalFiles.length}`,
 				"proc-zip-archive"
 			);
 			zip.add(fflateFile);
@@ -226,16 +354,152 @@ export default class DiffZipBackupPlugin extends Plugin {
 		fflateFile.push(t.encode("```\n" + stringifyYaml(toc) + "\n```"), true);
 		zip.end();
 	}
+	async extract(zipFile: string, extractFile: string, restoreAs: string) {
+		const zipPath = normalizePath(`${this.settings.backupFolder}/${zipFile}`);
+		const zipF = this.app.vault.getAbstractFileByPath(zipPath);
+		let files = [];
+		if (zipF instanceof TFile) {
+			files = [zipF]
+		} else {
+			let hasNext = true;
+			let counter = 0;
+			do {
+				counter++;
+				const zipF = this.app.vault.getAbstractFileByPath(zipPath + "." + `00${counter}`.slice(-3));
+				if (zipF instanceof TFile) {
+					files.push(zipF);
+				} else {
+					hasNext = false;
+				}
+			} while (hasNext)
+		}
+		if (files.length == 0) {
+			this.logMessage("Archived ZIP files were not found!");
+		}
+		const unzipper = new fflate.Unzip();
+		unzipper.register(fflate.UnzipInflate);
+		let extracted = false;
+		unzipper.onfile = file => {
+			// file.name is a string, file is a stream
+			if (file.name == extractFile) {
+				this.logMessage(
+					`${file.name} Found`,
+					"proc-zip-export"
+				);
+				file.ondata = async (_, dat, __) => {
+					extracted = true;
+					// Stream output here
+					this.logMessage(
+						`${file.name} Read`,
+						"proc-zip-export"
+					);
+					if (await this.writeFile(restoreAs, dat.buffer)) {
+						this.logMessage(
+							`${file.name} has been overwritten!`,
+							"proc-zip-export"
+						);
 
+					} else {
+						this.logMessage(
+							`Creating or Overwriting ${file.name} has been failed!`,
+							"proc-zip-export"
+						);
+					}
+				};
+
+				this.logMessage(
+					`${file.name} Reading...`,
+					"proc-zip-export"
+				);
+
+				file.start();
+
+			} else {
+				// this.logMessage(
+				// 	`${file.name} Skipped`,
+				// 	"proc-zip-export-skip"
+				// );
+			}
+		};
+		let idx = 0;
+		for (const f of files) {
+			idx++;
+			this.logMessage(
+				`Processing ${f.name}...`,
+				"proc-zip-export-processing"
+			);
+			const buf = new Uint8Array(await this.app.vault.readBinary(f));
+			const step = 1024 * 1024; // Possibly fails
+			let i = 0;
+			while (i < buf.byteLength) {
+				const isCompleted = extracted || (i + step > buf.byteLength && idx == files.length);
+				unzipper.push(buf.slice(i, i + step), isCompleted);
+				if (extracted) break;
+				i += step;
+			}
+			if (extracted) break;
+		}
+		this.logMessage(
+			`All ZIP files has been read.`,
+			"proc-zip-export-processing"
+		);
+		// unzipper.push(new Uint8Array(), true);
+	}
+	async onLayoutReady() {
+		if (this.settings.startBackupAtLaunch) {
+			this.createZip(false);
+		}
+	}
 	async onload() {
 		await this.loadSettings();
+		this.app.workspace.onLayoutReady(() => this.onLayoutReady())
 		this.addCommand({
 			id: "create-diff-zip",
-			name: "Create Differential Backup Zip",
+			name: "Create Differential Backup",
 			callback: () => {
-				this.createZip();
+				this.createZip(true);
 			},
 		});
+		this.addCommand({
+			id: "find-from-backups",
+			name: "Restore from backups",
+			callback: async () => {
+				const files = await this.loadTOC();
+				const filenames = Object.entries(files).sort((a, b) => b[1].mtime - a[1].mtime).map(e => e[0]);
+				const selected = await askSelectString(this.app, "Select file", filenames);
+				if (!selected) {
+					return;
+				}
+				const revisions = files[selected].history;
+				const d = `\u{2063}`;
+				const revisionList = revisions.map(e => `${e.zipName}${d} (${e.modified})`).reverse();
+				const selectedTimestamp = await askSelectString(this.app, "Select file", revisionList);
+				if (!selectedTimestamp) {
+					return;
+				}
+				const [filename,] = selectedTimestamp.split(d);
+				const suffix = filename.replace(".zip", "");
+				// No cares about without extension
+				const extArr = selected.split(".");
+				const ext = extArr.pop();
+				const selectedWithoutExt = extArr.join(".");
+				const RESTORE_OVERWRITE = "Original place and okay to overwrite";
+				const RESTORE_TO_RESTOREFOLDER = "Under the restore folder";
+				const RESTORE_WITH_SUFFIX = "Original place but with ZIP name suffix";
+				const restoreMethods = [RESTORE_TO_RESTOREFOLDER, RESTORE_OVERWRITE, RESTORE_WITH_SUFFIX]
+				const howToRestore = await askSelectString(this.app, "Where to restore?", restoreMethods);
+				const restoreAs = howToRestore == RESTORE_OVERWRITE ? selected : (
+					howToRestore == RESTORE_TO_RESTOREFOLDER ? normalizePath(`${this.settings.restoreFolder}/${selected}`) :
+						((howToRestore == RESTORE_WITH_SUFFIX) ? `${selectedWithoutExt}-${suffix}.${ext}` : "")
+				)
+				if (!restoreAs) {
+					return;
+				}
+
+				await this.extract(filename, selected, restoreAs);
+
+			},
+		})
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
@@ -266,10 +530,20 @@ class SampleSettingTab extends PluginSettingTab {
 		const { containerEl } = this;
 
 		containerEl.empty();
-
 		new Setting(containerEl)
-			.setName("Backup dir")
-			.setDesc("Folder to keep Backup ZIP and information file")
+			.setName("Start backup at launch")
+			// .setDesc("")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.startBackupAtLaunch)
+					.onChange(async (value) => {
+						this.plugin.settings.startBackupAtLaunch = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		new Setting(containerEl)
+			.setName("Backup folder")
+			.setDesc("Folder to keep each backup ZIPs and information file")
 			.addText((text) =>
 				text
 					.setPlaceholder("backup")
@@ -279,10 +553,33 @@ class SampleSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					})
 			);
+		new Setting(containerEl)
+			.setName("Restore folder")
+			.setDesc("Folder to save the restored file")
+			.addText((text) =>
+				text
+					.setPlaceholder("restored")
+					.setValue(this.plugin.settings.restoreFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.restoreFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		new Setting(containerEl)
+			.setName("Include hidden folders")
+			.setDesc("node_modules, .git, and trash of Obsidian are ignored automatically")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.includeHiddenFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.includeHiddenFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName("Source Size Cap")
-			.setDesc("(MB) Total size to stop zipping")
+			.setName("ZIP splitting size")
+			.setDesc("(MB) Size to split backup zip file")
 			.addText((text) =>
 				text
 					.setPlaceholder("30")
@@ -294,3 +591,50 @@ class SampleSettingTab extends PluginSettingTab {
 			);
 	}
 }
+
+
+export class PopoverSelectString extends FuzzySuggestModal<string> {
+	app: App;
+	callback?: (e: string) => void = () => { };
+	getItemsFun: () => string[] = () => {
+		return ["yes", "no"];
+
+	}
+
+	constructor(app: App, note: string, placeholder: string | null, getItemsFun: () => string[], callback: (e: string) => void) {
+		super(app);
+		this.app = app;
+		this.setPlaceholder((placeholder ?? "y/n) ") + note);
+		if (getItemsFun) this.getItemsFun = getItemsFun;
+		this.callback = callback;
+	}
+
+	getItems(): string[] {
+		return this.getItemsFun();
+	}
+
+	getItemText(item: string): string {
+		return item;
+	}
+
+	onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): void {
+		// debugger;
+		this.callback?.(item);
+		this.callback = undefined;
+	}
+	onClose(): void {
+		setTimeout(() => {
+			if (this.callback != undefined) {
+				this.callback("");
+			}
+		}, 100);
+	}
+}
+
+export const askSelectString = (app: App, message: string, items: string[]): Promise<string> => {
+	const getItemsFun = () => items;
+	return new Promise((res) => {
+		const popover = new PopoverSelectString(app, message, "", getItemsFun, (result) => res(result));
+		popover.open();
+	});
+};
